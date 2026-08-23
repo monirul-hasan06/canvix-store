0606426d9fd7dad6ce97ff27e056c4c5b3dc0218
Monirul Hasan Mithu <monirul.hasan513@gmail.com>
2026-08-23 15:05:38 +0600

    updated api

--- api/[...path].ts
+++ api/[...path].ts
@@ -1,3 +1,14 @@
-import app from "../server/server";
+import type { IncomingMessage, ServerResponse } from "node:http";
 
-export default app;
+type VercelRequest = IncomingMessage & { body?: unknown };
+type VercelResponse = ServerResponse & { status: (code: number) => VercelResponse; json: (body: unknown) => void };
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+	try {
+		const { default: app } = await import("../server/server");
+		return app(req, res);
+	} catch (error) {
+		console.error("API startup failed", error);
+		return res.status(500).json({ error: "API startup failed." });
+	}
+}
